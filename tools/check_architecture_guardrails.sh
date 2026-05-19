4b1dfd1742c26a49e233316d64b706970446cc77
Milky2018 <842376130@qq.com>
2026-05-19 21:09:00 +0800

    Move local declaration order into body plan

--- tools/check_architecture_guardrails.sh
+++ tools/check_architecture_guardrails.sh
@@ -643,6 +643,10 @@ if ! rg -n 'contains_materialized_expression|temporary_index' ir/wgsl_emit_expre
   fail "WGSL function-body arena plan must own materialized expression membership and temp index calculation"
 fi
 
+if ! rg -n 'local_declaration_order|needs_blank_after_local_declarations' ir/wgsl_emit_expression_temp_plan.mbt ir/wgsl_emit_functions.mbt >/dev/null; then
+  fail "WGSL function local declaration order must be owned by the function-body arena plan"
+fi
+
 if rg -n 'WgslIrFunctionScope::function_expression_temporary_name|scope\.function_expression_temporary_name|baked_function_expressions|record_baked_function_expression|projected_temporary_name_offset|hidden_temporary_name_indices|hide_temporary_name_indices|record_projected_temporary_expression' \
   ir --glob '*.mbt' >"$matches_file"; then
   cat "$matches_file" >&2
